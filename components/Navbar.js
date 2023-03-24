c1e7beb33882c5b190a8b5e867f111c2984c4e8b
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-03-24 11:58:34 +0100

    Ajout de Framer Motion pour animation entre les pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,26 +2,35 @@ import Link from "next/link";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { AnimatePresence, motion } from "framer-motion";
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <ul className="nav-elements">
-        <li>
-          <Link href="/">
-            <FontAwesomeIcon icon={faHome} />
-          </Link>
-        </li>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/about">A propos</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        className="navbar"
+        initial={{ opacity: 0, y: -15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -15 }}
+        transition={{ delay: 0.35 }}
+      >
+        <ul className="nav-elements">
+          <li>
+            <Link href="/">
+              <FontAwesomeIcon icon={faHome} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="/about">À propos</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import DynamicText from "@/components/DynamicText";
 import ImageCarousel from "@/components/ImageCarousel";
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const index = () => {
   return (
-    <div className="home_container">
-      <ImageCarousel />
-      <DynamicText />
-    </div>
+    <AnimatePresence>
+      <motion.div
+        className="home_container"
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 15 }}
+        transition={{ delay: 0.2 }}
+      >
+        <ImageCarousel />
+        <DynamicText />
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
