55efc89828fcb1a17b53405236ee3e96d8ce026a
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-03-25 12:02:01 +0100

    ajout classe active navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 const Navbar = () => {
+  const router = useRouter();
+  const isActive = (route) => router.pathname === route;
+
   return (
     <AnimatePresence>
       <motion.div
@@ -14,18 +18,18 @@ const Navbar = () => {
         transition={{ delay: 0.35 }}
       >
         <ul className="nav-elements">
-          <li>
+          <li className={isActive("/") ? "active" : ""}>
             <Link href="/">
               <FontAwesomeIcon icon={faHome} />
             </Link>
           </li>
-          <li>
+          <li className={isActive("/portfolio") ? "active" : ""}>
             <Link href="/portfolio">Portfolio</Link>
           </li>
-          <li>
+          <li className={isActive("/about") ? "active" : ""}>
             <Link href="/about">À propos</Link>
           </li>
-          <li>
+          <li className={isActive("/contact") ? "active" : ""}>
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
+import React from "react";
 import DynamicText from "@/components/DynamicText";
 import ImageCarousel from "@/components/ImageCarousel";
-import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 const index = () => {
