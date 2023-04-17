97c6c1dbcbb0087c3ad88c89d23c92ad1ed4000f
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-04-17 13:45:31 +0200

    SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import Layout from "@/components/Layout";
 import "../styles/index.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
     <Layout>
-      <title>Portfolio | Maxime Bignolet | Développeur Front-end</title>
+      <Head>
+        <title>Portfolio | Maxime Bignolet | Développeur Front-end</title>
+        <meta
+          name="description"
+          content="Portfolio de Maxime Bignolet, développeur front-end spécialisé en React, Next.js et autres technologies web modernes."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://your-domain.com/" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
 }
+
+export default App;
