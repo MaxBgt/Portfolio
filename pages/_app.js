14c34b59258af4959bcbc2d5f58a13e07cf06cfd
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-04-17 13:47:03 +0200

    SEO V2

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://your-domain.com/" />
+        <link rel="canonical" href="https://maxime-bignolet-portfolio.com/" />
       </Head>
       <Component {...pageProps} />
     </Layout>
